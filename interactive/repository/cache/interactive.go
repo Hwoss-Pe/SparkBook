f894c781b9d276c39a5d8ff8fef70b2217dea707
Hwoss <306031875@qq.com>
2025-01-11 14:38:53 +0800

    接入缓存，用HMSet进行存储对应的文章数据

--- interactive/repository/cache/interactive.go
+++ interactive/repository/cache/interactive.go
@@ -1,1 +1,101 @@
 package cache
+
+import (
+	"Webook/interactive/domain"
+	_ "embed"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"golang.org/x/net/context"
+	"strconv"
+	"time"
+)
+
+var (
+	//go:embed lua/interactive_incr_cnt.lua
+	luaIncrCnt     string
+	ErrKeyNotExist = redis.Nil
+)
+
+const (
+	fieldReadCnt    = "read_cnt"
+	fieldCollectCnt = "collect_cnt"
+	fieldLikeCnt    = "like_cnt"
+)
+
+//go:generate mockgen -source=./interactive.go -package=cachemocks -destination=mocks/interactive.mock.go InteractiveCache
+type InteractiveCache interface {
+	// IncrReadCntIfPresent 如果在缓存中有对应的数据，就 +1
+	IncrReadCntIfPresent(ctx context.Context,
+		biz string, bizId int64) error
+	IncrLikeCntIfPresent(ctx context.Context,
+		biz string, bizId int64) error
+	DecrLikeCntIfPresent(ctx context.Context,
+		biz string, bizId int64) error
+	IncrCollectCntIfPresent(ctx context.Context, biz string, bizId int64) error
+	// Get 查询缓存中数据
+	Get(ctx context.Context, biz string, bizId int64) (domain.Interactive, error)
+	Set(ctx context.Context, biz string, bizId int64, intr domain.Interactive) error
+}
+
+type RedisInteractiveCache struct {
+	client     redis.Cmdable
+	expiration time.Duration
+}
+
+func (r *RedisInteractiveCache) key(biz string, bizId int64) string {
+	return fmt.Sprintf("interactive:%s:%d", biz, bizId)
+}
+func (r *RedisInteractiveCache) IncrReadCntIfPresent(ctx context.Context, biz string, bizId int64) error {
+	return r.client.Eval(ctx, luaIncrCnt, []string{r.key(biz, bizId)}, fieldReadCnt, 1).Err()
+}
+
+func (r *RedisInteractiveCache) IncrLikeCntIfPresent(ctx context.Context, biz string, bizId int64) error {
+	return r.client.Eval(ctx, luaIncrCnt, []string{r.key(biz, bizId)}, fieldLikeCnt, 1).Err()
+}
+
+func (r *RedisInteractiveCache) DecrLikeCntIfPresent(ctx context.Context, biz string, bizId int64) error {
+	return r.client.Eval(ctx, luaIncrCnt, []string{r.key(biz, bizId)}, fieldLikeCnt, -1).Err()
+}
+
+func (r *RedisInteractiveCache) IncrCollectCntIfPresent(ctx context.Context, biz string, bizId int64) error {
+	return r.client.Eval(ctx, luaIncrCnt, []string{r.key(biz, bizId)}, fieldCollectCnt, 1).Err()
+}
+
+func (r *RedisInteractiveCache) Get(ctx context.Context, biz string, bizId int64) (domain.Interactive, error) {
+	result, err := r.client.HGetAll(ctx, r.key(biz, bizId)).Result()
+	if err != nil {
+		return domain.Interactive{}, err
+	}
+	if len(result) == 0 {
+		return domain.Interactive{}, ErrKeyNotExist
+	}
+
+	readCnt, _ := strconv.ParseInt(result[fieldReadCnt], 10, 84)
+	likeCnt, _ := strconv.ParseInt(result[fieldLikeCnt], 10, 64)
+	collectCnt, _ := strconv.ParseInt(result[fieldCollectCnt], 10, 64)
+
+	return domain.Interactive{
+		BizId:      bizId,
+		CollectCnt: collectCnt,
+		LikeCnt:    likeCnt,
+		ReadCnt:    readCnt,
+	}, err
+}
+
+func (r *RedisInteractiveCache) Set(ctx context.Context, biz string, bizId int64, intr domain.Interactive) error {
+	key := r.key(biz, bizId)
+	err := r.client.HMSet(ctx, key, key,
+		fieldLikeCnt, intr.LikeCnt,
+		fieldCollectCnt, intr.CollectCnt,
+		fieldReadCnt, intr.ReadCnt).Err()
+	if err != nil {
+		return err
+	}
+	return r.client.Expire(ctx, key, time.Minute*15).Err()
+}
+
+func NewRedisInteractiveCache(client redis.Cmdable) InteractiveCache {
+	return &RedisInteractiveCache{
+		client: client,
+	}
+}
--- interactive/repository/interactive.go
+++ interactive/repository/interactive.go
@@ -1,10 +1,17 @@
 package repository
 
+import "C"
 import (
 	"Webook/interactive/domain"
+	"Webook/interactive/repository/cache"
+	"Webook/interactive/repository/dao"
+	"Webook/pkg/logger"
+	"errors"
+	"github.com/ecodeclub/ekit/slice"
 	"golang.org/x/net/context"
 )
 
+//go:generate mockgen -source=./interactive.go -package=repomocks -destination=mocks/interactive.mock.go InteractiveRepository
 type InteractiveRepository interface {
 	IncrReadCnt(ctx context.Context,
 		biz string, bizId int64) error
@@ -17,4 +24,121 @@ type InteractiveRepository interface {
 	Liked(ctx context.Context, biz string, id int64, uid int64) (bool, error)
 	Collected(ctx context.Context, biz string, id int64, uid int64) (bool, error)
 	GetByIds(ctx context.Context, biz string, ids []int64) ([]domain.Interactive, error)
+}
+
+type CachedReadCntRepository struct {
+	cache cache.InteractiveCache
+	dao   dao.InteractiveDAO
+	l     logger.Logger
+}
+
+func (c *CachedReadCntRepository) IncrReadCnt(ctx context.Context, biz string, bizId int64) error {
+	err := c.dao.IncrReadCnt(ctx, biz, bizId)
+	if err != nil {
+		return err
+	}
+	return c.cache.IncrReadCntIfPresent(ctx, biz, bizId)
+}
+
+func (c *CachedReadCntRepository) BatchIncrReadCnt(ctx context.Context, bizs []string, bizIds []int64) error {
+	return c.dao.BatchIncrReadCnt(ctx, bizs, bizIds)
+}
+
+func (c *CachedReadCntRepository) IncrLike(ctx context.Context, biz string, bizId, uid int64) error {
+	err := c.dao.InsertLikeInfo(ctx, biz, bizId, uid)
+	if err != nil {
+		return err
+	}
+	return c.cache.IncrLikeCntIfPresent(ctx, biz, bizId)
+}
+
+func (c *CachedReadCntRepository) DecrLike(ctx context.Context, biz string, bizId, uid int64) error {
+	err := c.dao.DeleteLikeInfo(ctx, biz, bizId, uid)
+	if err != nil {
+		return err
+	}
+	return c.cache.DecrLikeCntIfPresent(ctx, biz, bizId)
+}
+
+func (c *CachedReadCntRepository) AddCollectionItem(ctx context.Context, biz string, bizId, cid int64, uid int64) error {
+	err := c.dao.InsertCollectionBiz(ctx, dao.UserCollectionBiz{
+		Biz:   biz,
+		Cid:   cid,
+		BizId: bizId,
+		Uid:   uid,
+	})
+	if err != nil {
+		return err
+	}
+	return c.cache.IncrCollectCntIfPresent(ctx, biz, bizId)
+}
+
+func (c *CachedReadCntRepository) Get(ctx context.Context, biz string, bizId int64) (domain.Interactive, error) {
+	intr, err := c.cache.Get(ctx, biz, bizId)
+	//缓存策略，只缓存这篇文章的点赞和收藏，对于用户干了什么的操作缓存的命中不大
+	if err == nil {
+		return intr, nil
+	}
+	ie, err := c.dao.Get(ctx, biz, bizId)
+	//	这里多一个双写的策略，无论找到数据还是没找到都要进行回写
+	//并且回写失败其实并不关心
+	if err == nil || errors.Is(err, dao.ErrRecordNotFound) {
+		res := c.toDomain(ie)
+		if er := c.cache.Set(ctx, biz, bizId, res); er != nil {
+			c.l.Error("回写缓存失败",
+				logger.Int64("bizId", bizId),
+				logger.String("biz", biz),
+				logger.Error(er))
+		}
+		return res, nil
+	}
+	return domain.Interactive{}, err
+}
+
+func (c *CachedReadCntRepository) Liked(ctx context.Context, biz string, id int64, uid int64) (bool, error) {
+	_, err := c.dao.GetLikeInfo(ctx, biz, id, uid)
+	switch {
+	case err == nil:
+		return true, nil
+	case errors.Is(err, dao.ErrRecordNotFound):
+		return false, nil
+	default:
+		return false, err
+	}
+}
+
+func (c *CachedReadCntRepository) Collected(ctx context.Context, biz string, id int64, uid int64) (bool, error) {
+	_, err := c.dao.GetCollectionInfo(ctx, biz, id, uid)
+	switch {
+	case err == nil:
+		return true, nil
+	case errors.Is(err, dao.ErrRecordNotFound):
+		return false, nil
+	default:
+		return false, err
+	}
+}
+
+func (c *CachedReadCntRepository) GetByIds(ctx context.Context, biz string, ids []int64) ([]domain.Interactive, error) {
+	vals, err := c.dao.GetByIds(ctx, biz, ids)
+	if err != nil {
+		return nil, err
+	}
+	return slice.Map[dao.Interactive, domain.Interactive](vals,
+		func(idx int, src dao.Interactive) domain.Interactive {
+			return c.toDomain(src)
+		}), nil
+}
+
+func NewCachedReadCntRepository(cache cache.InteractiveCache, dao dao.InteractiveDAO, l logger.Logger) *CachedReadCntRepository {
+	return &CachedReadCntRepository{cache: cache, dao: dao, l: l}
+}
+func (c *CachedReadCntRepository) toDomain(intr dao.Interactive) domain.Interactive {
+	return domain.Interactive{
+		Biz:        intr.Biz,
+		BizId:      intr.BizId,
+		LikeCnt:    intr.LikeCnt,
+		CollectCnt: intr.CollectCnt,
+		ReadCnt:    intr.ReadCnt,
+	}
 }
